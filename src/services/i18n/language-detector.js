6ae29aa38ded7f7c84c303d67cc022b75fbfb32f
Mohammad Ashour <ashour.mohammad@gmail.com>
2019-12-24 13:18:18 +0100

    Fixed an issue where configured fallback locale wasn't being used when the current system locale was not supported

--- src/services/i18n/language-detector.js
+++ src/services/i18n/language-detector.js
@@ -1,16 +1,24 @@
-import * as Localization from 'expo-localization';
+import * as Localization from "expo-localization";
+import { fallback, supportedLocales } from "../../config/i18n";
 
 const languageDetector = {
-    type: 'languageDetector',
+    type: "languageDetector",
     async: true,
-    detect: (callback) => {
+    detect: callback => {
         // We will get back a string like "en-US". We
         // return a string like "en" to match our language
         // files.
-        callback(Localization.locale.split('-')[0]);
+        const systemLocale = Localization.locale.split("-")[0];
+
+        const usedLocale =
+            supportedLocales[systemLocale] === undefined
+                ? fallback
+                : systemLocale;
+
+        callback(usedLocale);
     },
-    init: () => { },
-    cacheUserLanguage: () => { },
+    init: () => {},
+    cacheUserLanguage: () => {},
 };
 
 export default languageDetector;
