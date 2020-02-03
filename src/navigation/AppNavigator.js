b2bd8b009ed2558bcaedaf471dd005fc27cfc713
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-02-03 10:13:11 +0200

    Testing i18n with bottom tab navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import lists from '../config/lists';
 import i18n, { t } from '../services/i18n';
@@ -9,26 +9,28 @@ import ListScreen from '../screens/ListScreen';
 import AddTodoScreen from '../screens/AddTodoScreen';
 
 function getListNavItems() {
-    return lists.reduce((items, name) => {
-        const stackNavigator = createStackNavigator({
-            [name]: ListScreen,
-            AddTodoScreen,
-        });
+  return lists.reduce((items, name) => {
+    const stackNavigator = createStackNavigator({
+      [name]: ListScreen,
+      AddTodoScreen
+    });
 
-        stackNavigator.navigationOptions = ({ navigation }) => ({
-            title: t(`lists:${navigation.state.routeName}`),
-        });
+    stackNavigator.navigationOptions = ({ navigation }) => ({
+      title: t(`lists:${navigation.state.routeName}`)
+    });
 
-        return { ...items, [name]: stackNavigator };
-    }, {});
-};
+    return { ...items, [name]: stackNavigator };
+  }, {});
+}
 
-const AppNavigator = createDrawerNavigator(
-    getListNavItems(),
-    {
-        contentComponent: DrawerContent,
-        drawerPosition: i18n.isRTL ? 'right' : 'left',
-    },
-);
+// const AppNavigator = createDrawerNavigator(getListNavItems(), {
+//     contentComponent: DrawerContent,
+//     drawerPosition: i18n.isRTL ? "right" : "left",
+// });
+
+const AppNavigator = createBottomTabNavigator({
+  List: ListScreen,
+  AddTodo: AddTodoScreen
+});
 
 export default AppNavigator;
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -13,134 +13,135 @@ import AddButton from '../components/AddButton';
 import ListOfTodos from '../components/ListOfTodos';
 
 class ListScreen extends Component {
-    static navigationOptions = ({ navigation }) => {
-        return {
-            title: t(`lists:${navigation.state.routeName}`),
-            headerLeft: <ListHeaderStart navigation={navigation} />,
-        };
-    };
+	static navigationOptions = ({ navigation }) => {
+		return {
+			title: t('lists:list'),
+			// title: t(`lists:${navigation.state.routeName}`),
+			headerLeft: <ListHeaderStart navigation={navigation} />
+		};
+	};
 
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            todos: [],
+		this.state = {
+			todos: [],
             isLoading: true,
-        };
+		};
 
-        const listName = props.navigation.state.routeName;
+		const listName = props.navigation.state.routeName;
 
-        this.repo = new ListRepo(listName);
+		this.repo = new ListRepo(listName);
 
-        this.willFocusSubscription = props.navigation.addListener(
-            'willFocus',
+		this.willFocusSubscription = props.navigation.addListener(
+			'willFocus',
             () => this.loadTodosWithIndicator(),
-        );
-    }
+		);
+	}
 
-    componentWillUnmount() {
-        this.willFocusSubscription.remove();
-    }
+	componentWillUnmount() {
+		this.willFocusSubscription.remove();
+	}
 
-    async loadTodosWithIndicator() {
-        this.setState({ isLoading: true });
+	async loadTodosWithIndicator() {
+		this.setState({ isLoading: true });
 
-        const todos = await this.getKeyedTodosArray();
+		const todos = await this.getKeyedTodosArray();
 
-        this.setState({ todos, isLoading: false });
-    }
+		this.setState({ todos, isLoading: false });
+	}
 
-    async getKeyedTodosArray() {
+	async getKeyedTodosArray() {
         return this.transformToKeyedArray(
             await this.repo.getTodos()
         );
+	}
+
+	refreshTodos = async () => {
+		const todos = await this.getKeyedTodosArray();
+
+		this.setState({ todos });
     }
 
-    refreshTodos = async () => {
-        const todos = await this.getKeyedTodosArray();
-
-        this.setState({ todos });
-    }
-
-    /**
-     * @param {Object<string, { id: string }} todos
-     * @return {Array<Object>}
-     */
-    transformToKeyedArray(todos) {
-        return Object.keys(todos)
+	/**
+	 * @param {Object<string, { id: string }} todos
+	 * @return {Array<Object>}
+	 */
+	transformToKeyedArray(todos) {
+		return Object.keys(todos)
             .reverse().map(id => ({ ...todos[id], key: id }));
-    }
+	}
 
     updateItem = async (item) => {
-        await this.repo.updateTodo(item);
+		await this.repo.updateTodo(item);
 
-        this.refreshTodos();
+		this.refreshTodos();
     }
 
     deleteItem = async (item) => {
-        await this.repo.deleteTodo(item);
+		await this.repo.deleteTodo(item);
 
-        this.refreshTodos();
+		this.refreshTodos();
     }
 
-    goToAddTodoScreen = () => {
-        this.props.navigation.navigate('AddTodoScreen', {
+	goToAddTodoScreen = () => {
+		this.props.navigation.navigate('AddTodoScreen', {
             listName: this.props.navigation.state.routeName,
-        });
+		});
     }
 
-    renderContent() {
-        if (this.state.todos.length === 0) {
-            return (
-                <Text style={styles.emptyListText}>
-                    {t('ListScreen:empty')}
-                </Text>
-            );
-        }
+	renderContent() {
+		if (this.state.todos.length === 0) {
+			return (
+				<Text style={styles.emptyListText}>
+					{t('ListScreen:empty')}
+				</Text>
+			);
+		}
 
-        return (
-            <ListOfTodos
-                todos={this.state.todos}
-                onItemUpdate={this.updateItem}
-                onItemDelete={this.deleteItem}
-            />
-        );
-    }
+		return (
+			<ListOfTodos
+				todos={this.state.todos}
+				onItemUpdate={this.updateItem}
+				onItemDelete={this.deleteItem}
+			/>
+		);
+	}
 
-    render() {
-        return (
-            <View style={styles.container}>
+	render() {
+		return (
+			<View style={styles.container}>
                 {this.state.isLoading
                     ?
-                    <ActivityIndicator />
+					<ActivityIndicator />
                     :
-                    this.renderContent()
+					this.renderContent()
                 }
 
-                <AddButton
-                    style={styles.addButton}
-                    onPress={this.goToAddTodoScreen}
-                />
-            </View>
-        );
-    }
+				<AddButton
+					style={styles.addButton}
+					onPress={this.goToAddTodoScreen}
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
+	container: {
+		flex: 1,
+		alignItems: 'center',
         justifyContent: 'center',
-    },
+	},
 
-    emptyListText: {
-        fontSize: 16,
+	emptyListText: {
+		fontSize: 16,
         marginHorizontal: 30,
-    },
+	},
 
-    addButton: {
-        position: 'absolute',
-        bottom: 30,
+	addButton: {
+		position: 'absolute',
+		bottom: 30,
         end: 30,
     },
 });
